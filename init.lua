cdf8dfe82e7b3987ed31f62a2ab0ceda2170a051
JJSax <5084938+JJSax@users.noreply.github.com>
2023-05-17 15:31:12 -0600

    Add new call into init

--- init.lua
+++ init.lua
@@ -3,12 +3,13 @@ local HERE = (...):gsub('%.[^%.]+$', '')
 
 local Set = require(HERE..".set")
 local Array = require(HERE..".array")
-local queue = require(HERE..".queue")
+local Queue = require(HERE..".queue")
 
 local tablua = {}
 
 tablua.Set = Set.new
 tablua.array = Array.new
+tablua.queue = Queue.new
 
 
 return tablua
